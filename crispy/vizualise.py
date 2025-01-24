6fa2eca7f3adc5a06c39b0fb37f32927028ee50a
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2025-01-24 15:56:35 +0100

    to plot selected grains

--- crispy/vizualise.py
+++ crispy/vizualise.py
@@ -2,7 +2,7 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-import numpy as np
+
 import crispy
 
 
@@ -41,10 +41,15 @@ def centroids(polycrystal):
     _crispy_styling()
     fig, ax = plt.subplots(subplot_kw=dict(projection="3d"), figsize=(9, 7))
     x, y, z = polycrystal.centroids.T
-    if hasattr(polycrystal.grains[0], 'rgb'):
-        color = [g.rgb[:,0] for g in polycrystal.grains]
+    if hasattr(polycrystal.grains[0], "rgb"):
+        color = [g.rgb[:, 0] for g in polycrystal.grains]
         ax.scatter3D(x, y, z, c=color, s=50)
-        axis = str(polycrystal._ipf_axes[0].round(3)).replace("  ", ",").replace("[", "").replace("]", "")
+        axis = (
+            str(polycrystal._ipf_axes[0].round(3))
+            .replace("  ", ",")
+            .replace("[", "")
+            .replace("]", "")
+        )
         coloring = "ipf (view-axis x,y,z = {})".format(axis)
     else:
         ax.scatter3D(x, y, z, c=range(len(x)), cmap="rainbow", s=50)
@@ -58,7 +63,7 @@ def centroids(polycrystal):
 
 
 def mesh(
-    polycrystal, neighbourhood=None
+    polycrystal, neighbourhood=None, select=None
 ):  # TODO: add args for surface/interiror grains, colorings, etc.
     """Plot the Voronoi tesselation of the polycrystal.
 
@@ -68,6 +73,8 @@ def mesh(
             with number -neighbourhood- and all of its neighbours are
             plotted. Default is None, in which case the entire polycrystal
             mesh is plotted.
+        select (:obj:`list` of :obj:`int`, optional): List of grain indices
+            to plot. Default is None.
 
     Returns:
         :obj:`matplotlib.figure.Figure`, :obj:`matplotlib.axes.Axes`
@@ -85,27 +92,41 @@ def mesh(
         local_geometry = polycrystal._extract_geom(grains_index)
         mesh = crispy.tesselate._build_mesh(*local_geometry)
         grains = polycrystal.grains[grains_index]
+    elif select is not None:
+        grains_index = select
+        local_geometry = polycrystal._extract_geom(grains_index)
+        mesh = crispy.tesselate._build_mesh(*local_geometry)
+        grains = polycrystal.grains[grains_index]
     else:
         mesh = polycrystal._mesh
         grains = polycrystal.grains
 
-    if hasattr(grains[0], 'rgb'):
+    if hasattr(grains[0], "rgb"):
         facecolors = []
         for i in range(len(mesh.cells_dict["polygon"])):
             gid = mesh.cell_data["grain_id"][0][i]
-            facecolors.append( tuple(polycrystal.grains[gid].rgb[:, 0]) + (1,) )
-        axis = str(polycrystal._ipf_axes[0].round(3)).replace(" ", ",").replace("[", "").replace("]", "")
+            facecolors.append(tuple(polycrystal.grains[gid].rgb[:, 0]) + (1,))
+        axis = (
+            str(polycrystal._ipf_axes[0].round(3))
+            .replace(" ", ",")
+            .replace("[", "")
+            .replace("]", "")
+        )
         coloring = "ipf (view-axis x,y,z = {})".format(axis)
     else:
         cmap = cm.rainbow
         norm = Normalize(vmin=0, vmax=polycrystal.number_of_grains)
-        facecolors = [cmap(norm(mesh.cell_data["grain_id"][0][i])) for i in range(len(mesh.cells_dict["polygon"]))]
+        facecolors = [
+            cmap(norm(mesh.cell_data["grain_id"][0][i]))
+            for i in range(len(mesh.cells_dict["polygon"]))
+        ]
         coloring = "grain id"
 
     triangles = [mesh.points[poly] for poly in mesh.cells_dict["polygon"]]
 
-
-    ax.add_collection(Poly3DCollection(triangles, alpha=0.3, linewidths=0, facecolors=facecolors))
+    ax.add_collection(
+        Poly3DCollection(triangles, alpha=0.3, linewidths=0, facecolors=facecolors)
+    )
 
     _snap_to_bounds(ax, polycrystal)
 
@@ -113,7 +134,7 @@ def mesh(
         title = f"Voronoi Tesselation of Grain {neighbourhood} and Neighbours"
     else:
         title = "Voronoi Tesselation of the Polycrystal"
-    
+
     title = f"{title}\nColoring by {coloring}"
     ax.set_title(title)
 
@@ -130,8 +151,8 @@ if __name__ == "__main__":
     pc = crispy.Polycrystal(
         os.path.join(crispy.assets._asset_path, "FeAu_0p5_tR_ff1_grains.h5"),
         group_name="Fe",
-        lattice_parameters=  [4.0493, 4.0493, 4.0493, 90.0, 90.0, 90.0] ,
-        symmetry=225
+        lattice_parameters=[4.0493, 4.0493, 4.0493, 90.0, 90.0, 90.0],
+        symmetry=225,
     )
 
     pc.tesselate()
@@ -142,5 +163,6 @@ if __name__ == "__main__":
     fig, ax = centroids(pc)
     plt.show()
     fig, ax = centroids(pc)
+    plt.show()
     plt.show()
     plt.show()
